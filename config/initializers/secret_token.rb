8871cad1fd5cd41dea89ce15fb413963c7b28458
Marco Schincaglia <mschincaglia@navionics.com>
2011-11-07 14:55:46 +0100

    Created new TelephoneDirectory application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TelephoneDirectory::Application.config.secret_token = '4a0cea4e45f5d7411b7b1f53c22d7b3aabf2853952d5df336b1c5bf168b2eb5317367ac2bbbae65d56d34f8fc3c5ad38a53773af85b2c9c5e49618de8abaab27'
