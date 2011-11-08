f573568db8e5c50a5b9c8a3b131fb96ac478a382
Marco Schincaglia <mschincaglia@navionics.com>
2011-11-08 13:58:21 +0100

    Added some seeds for entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Entry.count == 0
+  Entry.create([
+    {:first_name => "Marco", :last_name => "Schincaglia", :phone_number => "+39 12 123654"},
+    {:first_name => "Carlo", :last_name => "Rossi", :phone_number => "+1 34 654321"},
+    {:first_name => "Mario", :last_name => "Bianchi", :phone_number => "+39 56 789654"},
+  ])
+end
